ca712663e3713d0085b4ec56e739686e043bc5b6
MarlonSelvi <marlonsb0502@gmail.com>
2024-07-07 01:34:55 +0000

    Code challenge submission for question: 217 Contains Duplicate

---
+++ 217 Contains Duplicate/217contains-duplicate.java
@@ -0,0 +1,20 @@
+import java.util.HashMap;
+
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashMap<Integer,Integer> hash = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+			if (hash.containsKey(nums[i])) {
+				 return true;
+			} else {
+				hash.put(nums[i], 1);
+			}
+		}
+		
+		
+		
+		
+		return false;
+    }
+}
